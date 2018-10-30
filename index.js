a0bce003850f5c658d08d16b967b616174244784
Mike Curtin <curtinmjc@gmail.com>
2018-10-30 17:20:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function getFirstSelector(selector) {
+  //MJC change via adding comment
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document
+    .querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  var lis = document
+    .querySelectorAll('ul.ranked-list li');
+  for (var i = 0; i < lis.length; i++) {
+    var rank = parseInt(lis[i].innerHTML, 10);
+//    var rank = lis[i].innerHTML;
+    lis[i].innerHTML = (rank + n);
+  }
+}
+
+function deepestChild() {
+  
+}
