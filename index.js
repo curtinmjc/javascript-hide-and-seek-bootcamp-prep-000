5ba6edc96ddb7fa7517f12bdffef8f8ccf9c8dba
Mike Curtin <curtinmjc@gmail.com>
2018-10-30 17:46:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+function getFirstSelector(selector) {
+  //MJC change via adding comment
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document
+    .querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  var lis = document
+    .querySelectorAll('ul.ranked-list li');
+  for (var i = 0; i < lis.length; i++) {
+    var rank = parseInt(lis[i].innerHTML, 10);
+//    var rank = lis[i].innerHTML;
+    lis[i].innerHTML = (rank + n);
+  }
+}
+
+function deepestChild() {
+  let current = document.querySelector('div#grand-node');
+  let next = current.children[0];
+  while (next) {
+    current = next;
+    next = current.children[0];
+  }
+  return current;
+}
